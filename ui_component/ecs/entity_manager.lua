fe5ca1b01d089799b5e9d0ed48120558b67c2f31
u16kuma <u16.kuma@gmail.com>
2018-06-02 18:56:18 +0900

    不要なコメントアウトを削除

--- ui_component/ecs/entity_manager.lua
+++ ui_component/ecs/entity_manager.lua
@@ -50,41 +50,4 @@ function M.new()
 	return instance
 end
 
---[[
-function M.create_entity_manager()
-	local instance = {}
-	instance._entities = {}
-	instance.add = function(self, entity)
-		table.insert(self._entities, entity)
-	end
-	instance.remove = function(self, entity)
-		for i, value in ipairs(self._entities) do
-			if value == entity then
-				table.remove(self._entities, i)
-				return
-			end
-		end
-	end
-	instance.get = function(self, id)
-		for i, value in ipairs(self._entities) do
-			if value.id == id then
-				return value
-			end
-		end
-		return nil
-	end
-	instance.start = function(self)
-		for i = 1, #self._entities do
-			self._entities[i]:start()
-		end
-	end
-	instance.on_input = function(self, action_id, action)
-		for i = 1, #self._entities do
-			self._entities[i]:on_input(action_id, action)
-		end
-	end
-	return instance
-end
---]]
-
 return M
